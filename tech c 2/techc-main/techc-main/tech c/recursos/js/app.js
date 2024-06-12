a93044864506ed67132875aae63b333438b0c9c5
CarlosdePaul <164961699+CarlosdePaul@users.noreply.github.com>
2024-06-11 21:33:49 -0400

    adicionei login

---
+++ tech c 2/techc-main/techc-main/tech c/recursos/js/app.js
@@ -0,0 +1,8 @@
+
+function forgotpasswordModal(){
+	let fade = document.querySelector('#fade')
+	let modal_card= document.querySelector('modal_card')
+	
+	fade.classList.toggle('hide')
+	modal_card.classList.toggle('hide')
+}
