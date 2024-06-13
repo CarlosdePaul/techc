86b338df2203c4b02ee236903868b5410865b3a2
CarlosdePaul <164961699+CarlosdePaul@users.noreply.github.com>
2024-06-12 21:29:12 -0400

    Add files via upload

--- tech c 2/techc-main/techc-main/tech c/recursos/js/app.js
+++ tech c 2/techc-main/techc-main/tech c/recursos/js/app.js
@@ -1,8 +1,18 @@
 
 function forgotpasswordModal(){
 	let fade = document.querySelector('#fade')
-	let modal_card= document.querySelector('modal_card')
+	let modal_card= document.querySelector('#page')
 	
 	fade.classList.toggle('hide')
 	modal_card.classList.toggle('hide')
+}
+
+let cont=0
+
+function registerLogin(){
+	let login = localstorage.getItem("login")
+	if(login === null){
+		localStorage.setItem("login","icet2024")
+		
+	}
 }
